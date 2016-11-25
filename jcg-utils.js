5690be1c3971fc54966d4a1df5eba6aa52a45d05
James Go <jamescookgo@gmail.com>
2016-11-25 15:18:12 -0500

    added flaot regex checking function

--- jcg-utils.js
+++ jcg-utils.js
@@ -2,4 +2,9 @@ function deepObjectCopy(obj) {
     return JSON.parse(JSON.stringify(obj));
 }
 
-export { deepObjectCopy };
+function floatRegex(n) {
+    var regex = /^([0-9]*[.])?[0-9]*$/;
+    return regex.test(n);
+}
+
+export { deepObjectCopy, floatRegex };
